1fa91a006d2ef6ff18f6292b96f52db4cdb6f171
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-07 12:24:53 -0800

    Added mod support for the TeleportPad and Chisel mod

--- Better Categorization/ModChecker.cs
+++ Better Categorization/ModChecker.cs
@@ -14,10 +14,17 @@ namespace Better_Categorization
 
         public void AfterModsLoaded(List<ModLoader.ModDescription> mods)
         {
-            bool hasColoredSandMod = mods.Where(mod => mod.name == "Colored Sand").Any();
+            //bool hasColoredSandMod = mods.Where(mod => mod.name == "Colored Sand").Any();
 
+            // If the Colored Sand mod is installed...
+            if (mods.Where(mod => mod.name == "Colored Sand").Any()) 
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-colored_sand.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
 
-            if (hasColoredSandMod) ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-colored_sand.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
+            if (mods.Where(mod => mod.name == "Teleportpad").Any()|| mods.Where(mod => mod.name == "Teleport Pads+").Any()) 
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-teleportpad.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
+
+            if (mods.Where(mod => mod.name == "Chisel").Any()) 
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-chisel.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
         }
 
         public void OnAssemblyLoaded(string path)
