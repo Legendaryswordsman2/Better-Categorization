c5ffe63a1a22e9b0f3b67b2ba51d4a3ebac9d4f0
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-07 01:55:06 -0800

    Added mod support for the Colored Sand mod

---
+++ Better Categorization/ModChecker.cs
@@ -0,0 +1,28 @@
+﻿using Pipliz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better_Categorization
+{
+    class ModChecker : ModLoaderInterfaces.IAfterModsLoaded, ModLoaderInterfaces.IOnAssemblyLoaded
+    {
+        string assemblyPath;
+
+        public void AfterModsLoaded(List<ModLoader.ModDescription> mods)
+        {
+            bool hasColoredSandMod = mods.Where(mod => mod.name == "Colored Sand").Any();
+
+
+            if (hasColoredSandMod) ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-colored_sand.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
+        }
+
+        public void OnAssemblyLoaded(string path)
+        {
+            assemblyPath = path;
+        }
+    }
+}
