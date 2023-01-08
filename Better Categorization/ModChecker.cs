4396897284adbee611b029ed4901d945d9cc1d6f
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-01-07 22:15:51 -0800

    Added support for more mods

--- Better Categorization/ModChecker.cs
+++ Better Categorization/ModChecker.cs
@@ -18,27 +18,24 @@ namespace Better_Categorization
 
             // If the Colored Sand mod is installed...
             if (mods.Where(mod => mod.name == "Colored Sand").Any())
-                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-colored_sand.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 15000);
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-colored_sand.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
 
 
             //Log.Write((mods.Where(mod => mod.name == "TeleportPad").Any() == true).ToString());
             if (mods.Where(mod => mod.name == "TeleportPad").Any() || mods.Where(mod => mod.name == "Teleport Pads+").Any())
-                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-teleportpad.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 15000);
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-teleportpad.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
 
             if (mods.Where(mod => mod.name == "Chisel").Any())
-                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-chisel.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 15000);
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-chisel.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
 
             if (mods.Where(mod => mod.name == "Lanterns+").Any())
-                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-lanterns+.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 15000);
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-lanterns+.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
 
-            //for (int i = 0; i < mods.Count; i++)
-            //{
-            //    Log.Write(mods[i].name + " -----------------");
-            //}
-            //Log.Write((mods.Where(mod => mod.name == "Kings 0.9 Textures and Decorations").Any() == true).ToString());
+            if (mods.Where(mod => mod.name == "AdvancedForester").Any())
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-advanced_forester.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
 
-            //if (mods.Where(mod => mod.name == "Kings 0.9 Textures and Decorations").Any() == true)
-            //    ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-kings_0.9_textures_and_decorations.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 1000);
+            if (mods.Where(mod => mod.name == "Compass").Any())
+                ItemTypesServer.QueueItemTypePatches(Path.Combine(Path.GetDirectoryName(assemblyPath), "overridetypes-compass.json"), ItemTypesServer.EItemTypePatchType.OverrideTypeProperties, 50000);
         }
 
         public void OnAssemblyLoaded(string path)
